c6eef4080ac7ef6b502dcaf33ffde4e53c78f3ca
rohanmathur91 <mathurrohan04@gmail.com>
2022-06-27 17:20:06 +0530

    add counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./app.css";
 
 export const App = () => {
-  return <h1 className="header">learning web tooling</h1>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="header">learning web tooling</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increase count
+      </button>
+    </div>
+  );
 };
